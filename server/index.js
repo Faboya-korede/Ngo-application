b0951ab0c1ab567baf20b4e514b23329c2d95f37
Faboya Basil korede <106436258+Faboya-korede@users.noreply.github.com>
2023-06-05 01:49:21 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,18 +6,40 @@ let cookieParser = require('cookie-parser');
 let requireDir = require('require-dir');
 let routes = requireDir('./routes');
 let auth = require('./middleware/auth');
-let mongoose = require('mongoose');
+let MongoClient = require('mongodb').MongoClient;
+//let mongoose = require('mongoose');
 const chalk = require('chalk');
 require('dotenv').config();
 
 // connect to db
-mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/ngodb?authSource=admin', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-}).then(
-	() => console.log(chalk.yellow('Connected to mongo database at bukola.xyz')),
-	err => console.log(chalk.red(err))
-);
+//mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/ngodb?authSource=admin', {
+//	useNewUrlParser: true,
+//	useUnifiedTopology: true
+//}).then(
+//	() => console.log(chalk.yellow('Connected to mongo database at bukola.xyz')),
+//	err => console.log(chalk.red(err))
+//);
+
+//connect to db 
+// use when starting application locally with node command
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+
+// use when starting application as a separate docker container
+let mongoUrlDocker = "mongodb://admin:password@host.docker.internal:27017";
+
+// use when starting application as docker container, part of docker-compose
+let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
+
+let mongoUrlK8s = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/ngodb?authSource=admin`
+
+// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+// "user-account" in demo with docker. "my-db" in demo with docker-compose
+let databaseName = "my-db";
+
+MongoClient.connect(mongoUrlK8s, mongoClientOptions, function (err, client) {
+    if (err) throw err;
 
 // express setup
 let app = express();
